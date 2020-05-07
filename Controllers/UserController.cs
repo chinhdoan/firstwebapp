29f60a5fb731fd88ec6f454669fe701fbf110cd6
chinhdoan <31790367+chinhdoan@users.noreply.github.com>
2020-05-07 16:44:11 +0700

    Initial commit

---
+++ Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using Evncpc.Models;
+using Microsoft.VisualBasic.ApplicationServices;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace Evncpc.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult AddOrEdit(int id=0)
+        {
+            user userModel = new user();
+            return View(userModel);
+        }
+
+        [HttpPost]
+
+        public ActionResult AddOrEdit(user userModel)
+        {
+            using (DbModels dbmodel = new DbModels())
+            {
+                dbmodel.users.Add(userModel);
+                dbmodel.SaveChanges();
+            }
+            ModelState.Clear();
+            ViewBag.SuccessMessage = "Registion successfull";
+            return View("AddOrEdit", new user());
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Evncpc.MvcApplication" Language="C#" %>
